97b2756073263a399aa6b058f1b160a8c68773fb
Matheus Avila <matheus_avila17@hotmail.com>
2022-01-31 16:14:59 -0300

    Create user.js

---
+++ 04 - JavaScript/04 - Pegando Dados User/user.js
@@ -0,0 +1,7 @@
+/*
+alert("Bem vindo, a seguir pediremos que informe alguns dados")
+let name = prompt("Qual seu nome?")
+let age = prompt("Qual sua idade?")
+let ageconfirm = confirm("Confirme sua idade: " + age + " anos")
+alert("Nome: " + name + "\nIdade: " + age + "\nIdade confirmada: " + ageconfirm)
+*/
