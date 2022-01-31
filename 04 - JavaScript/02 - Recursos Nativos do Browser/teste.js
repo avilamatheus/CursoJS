a3b465ecc7118cf9a1f92b541faab9903c07b0de
Matheus Avila <matheus_avila17@hotmail.com>
2022-01-31 15:51:17 -0300

    Recursos Nativos do Browser

---
+++ 04 - JavaScript/02 - Recursos Nativos do Browser/teste.js
@@ -0,0 +1,10 @@
+//oq tiver comentado só vai funcionar no browser
+
+console.log("Hello Guys")
+console.info("WHATSUZP")
+console.warn("alerta")
+console.error("erro")
+
+//alert("Bem vindo!")
+//confirm("dale pau")
+//prompt("Qual seu nome?")
