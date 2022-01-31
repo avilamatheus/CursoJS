b0bbb17c95dfcdc854e8b9b71abf3c78f2d76aa3
Matheus Avila <matheus_avila17@hotmail.com>
2022-01-31 16:02:53 -0300

    Declarando Variaveis

---
+++ 04 - JavaScript/03 - Declarando Variaveis/variaveis.js
@@ -0,0 +1,26 @@
+let spaceship = "Chevrolet Celta"
+console.log(spaceship)
+spaceship = "Toyota Corolla"
+console.log(spaceship)
+
+const squad = "Lightyear"
+console.log(squad)
+//squad = "Buzz Lightyear" isso aqui vai dar erro pq é CONST!
+
+var velocidade = 80 //tldr igual a let
+console.log(velocidade)
+velocidade = 75.6
+console.log(velocidade)
+
+let novalue
+console.log(novalue) //retorna undefined nesse caso
+
+//isso aqui abaixo só vai funcionar no browser
+
+//let confirmation = confirm("Deseja confirmar essa ação?")
+//console.log(confirmation)
+//no caso o confirm vai retornar true se a pessoa clicar em ok e false se clicar em cancelar
+
+//let yourName = prompt("Digite seu nome:")
+//console.log(yourName)
+//no caso o prompt vai retornar o input. se clicar em cancelar vai retornar null
